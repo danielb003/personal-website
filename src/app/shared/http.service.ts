03e1d345b16a1c718483c24b2a44a6bf7a94a331
danielb003 <dbellino92@gmail.com>
2026-06-06 16:35:23 +1000

    modify api base url v4

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -10,7 +10,9 @@ import { environment } from '../../environments/environment.prod';
 
 export class HttpService {
    // private readonly apiUrl:string = "/api/send-email";
-   private emailUrl = `${environment.apiUrl}/api/send-email`;
+   // private emailUrl = `${environment.apiUrl}/api/send-email`;
+   private emailUrl = "/api/send-email";
+
 
    http: HttpClient = inject(HttpClient);
 
