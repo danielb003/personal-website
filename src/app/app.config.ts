73ae3083d6ea15feb82f8649db69caa72c3839fc
danielb003 <dbellino92@gmail.com>
2026-06-11 14:21:07 +1000

    import and implement vercel analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { inject } from '@vercel/analytics';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideAppInitializer(() => {
+      inject();
+    })
   ]
 };
